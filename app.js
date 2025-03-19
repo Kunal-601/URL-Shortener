42fb4fa92fd6a36037148f4ca4b645bd83d792ff
Kunal-601 <kunalkaushik601@gmail.com>
2025-03-19 12:09:17 +0530

    Added starting files for frontend and a test route for ejs home page

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import urlRouter from './routes/url.route.js';
 import userRouter from './routes/user.route.js';
+import path from 'path';
 
 
 const app = express();
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//route for testing home page ejs
+app.get('/test', (req, res)=>{
+    res.render('home');
+})
 //routes
 app.use('/api/urls', urlRouter);
 app.use('/api/users', userRouter);
